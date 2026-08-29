52a8a29c41235d8a0862ec0c419abc02a401f9bb
ki30306272200455@depi.eui.edu.eg <ki30306272200455@depi.eui.edu.eg>
2026-08-29 13:52:27 +0000

    account status upper logic

--- fintech_dwh/models/staging/stg_accounts.sql
+++ fintech_dwh/models/staging/stg_accounts.sql
@@ -2,7 +2,7 @@ select
     account_id,
     customer_id,
     initcap(account_type)                  as account_type,
-    upper(account_status)                  as account_status,
+    initcap(account_status)                as account_status,
     upper(currency)                        as currency,
     cast(created_at as timestamp)          as created_at,
     cast(updated_at as timestamp)          as updated_at
